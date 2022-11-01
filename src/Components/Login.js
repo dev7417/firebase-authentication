05f500fbde6a8775a80b5859456f9063a62f75d1
Dev <gangwardev50@gmail.com>
2022-11-01 16:32:04 +0530

    adding firebase to our login page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,41 +1,64 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { auth } from '../Firebase'
 import './contactForm.css'
 import { useState } from 'react';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import { Box } from '@mui/material'
 
 export default function Login() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    // const [show, setShow] = useState(false)
+    const [status, setStatus] = useState({
+        status: true,
+        type: '',
+        msg: ''
+    })
 
-    const handleLogin = (e) =>{
+    const handleLogin = async (e) => {
         e.preventDefault();
-        console.log(email, password)
+        // console.log(email, password)
+        try {
+            const result = await auth.createUserWithEmailAndPassword(email, password);
+            console.log(result);
+            setStatus({ status: true, type: 'success', msg: 'data submitted successfully' });
+
+
+        } catch (error) {
+            console.log(error.FirebaseError);
+            setStatus({ status: true, type: 'error', msg: error.FirebaseError })
+        }
     }
+
     return (
+
         <>
             <div className='container'>
                 <div className='login-form'>
                     <div className='m-form'>
-                        <Form onClick={handleLogin}>
+                        <Form onSubmit={handleLogin}>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label style={{fontWeight:'bold', fontSize:'25px', fontFamily:'Nunito Sans'}}>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" onChange={(e)=>setEmail(e.target.value)} />
+                                <Form.Label style={{ fontWeight: 'bold', fontSize: '25px', fontFamily: 'Nunito Sans' }}>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
                             </Form.Group>
 
                             <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label style={{fontWeight:'bold', fontSize:'25px', fontFamily:'Nunito Sans'}}>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                                <Form.Label style={{ fontWeight: 'bold', fontSize: '25px', fontFamily: 'Nunito Sans' }}>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
                             </Form.Group>
-                            <Button variant="primary" type="submit" className='mx-auto d-flex' style={{fontWeight:'bold'}}>
+                            <Button variant="primary" type="submit" className='mx-auto d-flex' style={{ fontWeight: 'bold' }}>
                                 Submit
                             </Button>
                         </Form>
                     </div>
 
                 </div>
-
             </div>
+            <Alert severity={status.type}>{status.msg}</Alert>
+
 
 
         </>
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,10 @@
-import firebase from 'firebase/app'
-import 'firebase/auth';
-import 'firebase/firebase-firestore'
-import { Config } from './Config';
-
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore';
+import  'firebase/compat/auth';
+import Config from './Config';
 
 firebase.initializeApp(Config)
-
 const auth = firebase.auth();
 const db = firebase.firestore()
 
-
-export   {auth, db}
+export {auth, db}
